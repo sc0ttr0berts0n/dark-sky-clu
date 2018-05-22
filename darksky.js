010a93ea01c50b5755a352eb76bb0dd1a2b70472
Scott Robertson <robertsons@marketingresults.net>
2018-05-22 11:38:10 -0400

    initial commit\!

---
+++ darksky.js
@@ -0,0 +1,40 @@
+require('dotenv').config({
+    path: '/Users/robertsons/Desktop/drop/test/weather/.env'
+});
+
+const DarkSky = require('dark-sky');
+const darksky = new DarkSky(process.env.DARK_SKY_API_KEY); // Your API KEY can be hardcoded, but I recommend setting it as an env variable.
+
+darksky
+    .options({
+        latitude: process.env.LAT,
+        longitude: process.env.LON,
+        language: 'en',
+        exclude: ['minutely', 'alerts', 'flags']
+    })
+    .get()
+    .then(function(data) {
+        let currently = data.currently;
+        let today = data.daily.data[0];
+        let weekly = data.daily;
+
+        // Currently
+        console.log('\nCURRENTLY\n---------');
+        console.log(currently.summary);
+        console.log('Temperature: ' + Math.round(currently.temperature) + '°F');
+        console.log(
+            'Feels Like: ' + Math.round(currently.apparentTemperature) + '°F'
+        );
+        console.log('Chance of Rain: ' + currently.precipProbability + '%');
+
+        // Today
+        console.log('\nTODAY\n-----');
+        console.log(data.hourly.summary);
+        console.log('Low: ' + Math.round(today.temperatureLow) + '°F');
+        console.log('High: ' + Math.round(today.temperatureHigh) + '°F');
+        console.log('Chance of Rain: ' + today.precipProbability + '%');
+
+        // This Week
+        console.log('\nTHIS WEEK\n---------');
+        console.log(weekly.summary);
+    });
